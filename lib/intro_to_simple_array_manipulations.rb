7f9ad67c4000bce55ee11ca37e6cdd4e89ddba18
Mendy <mendybeltcohen@gmail.com>
2018-08-12 22:35:29 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,9 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat(array, arrays)
+def using_concat(array1, array2)
    array += arrays
 end
+
+def using_insert(array1, array2)
+  
